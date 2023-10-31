b803fe8fec235677fa19b1713d0dc20f6ffc5d5f
JDD <jose.drouilly@usach.cl>
2023-10-31 19:51:33 -0300

    add info for posts and make visible in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,49 @@ import Head from "next/head";
 import Link from "next/link";
 import { use } from "react";
 
-import { api } from "run/utils/api";
+import { RouterOutputs, api } from "run/utils/api";
+
+const CreatePostWizard = () => {
+
+  const { user } = useUser();
+
+  console.log(user);
+
+  if (!user) return null;
+
+  return <div className="flex w-full gap-3">
+    <img
+      src={user.imageUrl}
+      alt="Profile Image"
+      className="w-14 h-14 rounded-full" />
+    <input placeholder="type something" className="grow bg-transparent outline-none" />
+  </div>
+
+}
 
 export default function Home() {
-
-  const CreatePostWizard = () => {
-
-    const { user } = useUser();
-
-    if(!user) return null;
-
-    return <div className="flex gap-3">
-      <img 
-        src={user.imageUrl} 
-        alt="Profile Image" 
-        className="w-14 h-14 rounded-full"/>
-        <input placeholder="type something" className="bg-transparent"/>
-    </div>
-
-  }
 
   const user = useUser();
 
   const { data, isLoading } = api.posts.getAll.useQuery();
 
-  if(isLoading) return <div>Loading ....</div>
+  if (isLoading) return <div>Loading ....</div>
 
-  if(!data) return <div>something went wrong</div>
+  if (!data) return <div>something went wrong</div>
 
+  type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+  const PostView = (props: PostWithUser) => {
+    const { posts, author } = props;
+
+    return (
+      <div key={posts.id} className="border-b p-8 border-slate-400">
+        <img src={author.profilePicture} className="w-14 h-14 rounded-full" />
+        {posts.content}
+      </div>
+    );
+    
+
+  }
 
   return (
     <>
@@ -43,16 +58,16 @@ export default function Home() {
       <main className="flex h-screen justify-center">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            {!user.isSignedIn && 
+            {!user.isSignedIn &&
               <div className="flex justify-center">
                 <SignInButton />
               </div>
             }
-            {!user.isSignedIn && <CreatePostWizard/>}
+            {user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {[...data, ...data]?.map((posts) => (
-              <div key={posts.id} className="boreder-b p-8 border-slate-400">{posts.content}</div>
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.posts.id} />
             ))}
           </div>
 
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,28 +2,43 @@ import { z } from "zod";
 
 
 import { createTRPCRouter, publicProcedure } from "run/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs/server";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
+import type { Post } from "@prisma/client";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.imageUrl,
+  };
+}
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-     return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((posts) => posts.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log(users);
+
+    return posts.map((posts) => {
+      const author = users.find((user) => user.id === posts.authorId);
+
+      if(!author) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "author for post not found"});
+
+      return {
+        posts,
+        author,
+      };
+    });
   }),
-
-  // create: publicProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     return ctx.db.post.create({
-  //       data: {
-  //         name: input.name,
-  //       },
-  //     });
-  //   }),
-
-  // getLatest: publicProcedure.query(({ ctx }) => {
-  //   return ctx.db.post.findFirst({
-  //     orderBy: { createdAt: "desc" },
-  //   });
-  // }),
 });
